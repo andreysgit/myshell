fef56df78ebd3bedd9e9c67a48ae93e77b4bf589
andrey <smootykins@gmail.com>
2019-10-15 19:50:20 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+ * a text line>
+ * input
+ * Pass by ref
+ * The reason to do this is so that
+ * the function will have the power to change the value.
+ *
+ *
+ */
+
+void clear(){
+    printf("\033[H\033[J");
+}
+
+void parse(char *line){
+
+
+//    for (int i = 0; i < 99; i++) {
+//        while ((argArray[i] = strtok_r(line, " ", &line))){
+//            printf("%s ", argArray[i]);
+//        }
+//    }
+
+
+}
+
+void read(void){
+
+}
+
+void run(char** args){
+
+}
+
+//main loop
+
+void loop() {
+
+    char *argArray[99];
+    cout << "User>";
+    cin >>
+//    cin.getline(myline,99);
+//    parse(myline);
+//    clear();
+
+//    char *line = NULL;
+//    string **args = NULL;
+//    int status;
+    //user enters commands
+    //parsing happens to separate out arguments from command
+    //execute command
+//    status=1;
+
+//    while(status)
+//        parse(line);
+//        parse(line);
+//       status = run(args);
+
+
+//        status=0;
+
+    }
+
+
+int main(int argc, char** argv) {
+
+    //while
+    loop();
+
+    return 0;
+}
+
+
+
