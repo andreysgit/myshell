0056b166705b1455f0cc87b5375b7b3ba1a283f9
andrey <smootykins@gmail.com>
2019-10-18 12:52:31 -0400

    working: main loop echo clear pause print environment
    todo:
    cd

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <cstring>
 #include <cstdio>
+#include <string>
+#include <zconf.h>
+#include <errno.h>
 
 
 using namespace std;
@@ -28,12 +31,12 @@ void clear(){
     printf("\033[H\033[J\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 
-void pause(){
+void paused(){
 
     printf("Shell is paused. Press enter to continue");
     while (cin.get()!='\n'){
         }
-    printf("Shell unpaused");
+    printf("Shell unpaused\n");
 
 }
 
@@ -46,46 +49,82 @@ void printUsrPrompt(char** argv){
     printf("<%s/SHELL>",argv[0]);
 };
 
-void tokenize(char *line, char *array[]){
+int tokenize(char *line, char *array[]){
 
     int i=0;
     char* token=NULL;
     token=strtok(line," ");
     array[i]=token;
+    int numArgs;
 
     if(token!=NULL){
         while(token!=NULL){
             array[i]=token; i++;
             token=strtok(NULL," ");
+            numArgs++;
         }
     }
 
     printf("Tokenized: \n");
-    for (int k = 0; array[k]!=0 ; k++) {
-        printf("%s,", array[k]);
-
+    for (int j = 0; j < numArgs; j++) {
+        printf("%d - %s\n",j, array[j]);
     }
-
+    printf("number of arguments: %d \n\n", numArgs);
+    return numArgs;
 }
 
 void execute(char* pString[]) {
-    printf("gets to echo");
-//    for (size_t i = 0; i < 99; i++) {
-//        printf("%s,",pString[i]);
-//    }
+
 }
 
-void echo(char *str[]){
-    printf("Got to echo");
-    for (int k = 1; str[k]!=0 ; k++) {
-        printf("%s ", str[k]);
+void echo(char *array[],int numArgs){
+    printf("Got to echo\n");
+    for (int j = 1; j < numArgs; j++) {
+        printf("%s ",array[j]);
     }
 }
-void parse(char *array[]){
-    printf("Got to parse \n");
-    if (strncmp(array[0], "echo", 4) == 0){
-        echo(array);
+
+void cd(char *array[],int numArgs){
+    printf("Change directory\n");
+    int error;
+    if(numArgs==1){
+        char cwd[PATH_MAX];
+        printf("%s", getcwd(cwd, sizeof(cwd)));
     }
+    if(numArgs==2){
+        error = chdir(array[1]);
+        printf("chdir error: %d. errorno = %d",error,errno);
+    }
+    if(numArgs>2){
+    }
+
+}
+
+int parse(char *array[],int numArgs){
+    printf("Parsing \n");
+
+    if (strncmp(array[0], "echo", 4) == 0){
+        echo(array,numArgs);
+    }
+    if (strncmp(array[0], "clear", 5) == 0){
+        clear();
+    }
+
+    if (strncmp(array[0], "cd", 5) == 0){
+        cd(array,numArgs);
+    }
+
+    if (strncmp(array[0], "pause", 5) == 0){
+        paused();
+    }
+
+    if (strncmp(array[0], "quit", 4) == 0){
+        printf("Quit function registered. Exiting program");
+        return(1);
+    }
+
+    else return 0;
+
 
 }
 int main(int argc, char** argv, char** envp) {
@@ -94,11 +133,15 @@ int main(int argc, char** argv, char** envp) {
     printUsrPrompt(argv);
     char* userInputString=NULL;
     size_t buff = 0;
-    getline(&userInputString, &buff,stdin);
-    tokenize(userInputString, array);
-    parse(array);
+    int numArgs;
 
-
+    do{
+        printUsrPrompt(argv);
+        getline(&userInputString, &buff,stdin);
+        size_t sizeOfArray = (sizeof(array) / sizeof(array[0]));
+        numArgs = tokenize(userInputString, array);
+    }
+    while(parse(array,numArgs)!=1);
 
     return 0;
 }
